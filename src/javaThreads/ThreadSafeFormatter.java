c3c0b064d373903f279d8f17721117215dfadd21
v.karthigeyan@hotmail.com <[1flowersday1]>
2020-02-03 02:55:13 +0530

    Callable future

---
+++ src/javaThreads/ThreadSafeFormatter.java
@@ -0,0 +1,34 @@
+package javaThreads;
+
+import javax.xml.bind.annotation.XmlInlineBinaryData;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ThreadSafeFormatter {
+
+    public static ThreadLocal<SimpleDateFormat> dateFormatThreadLocal = new ThreadLocal<SimpleDateFormat>(){
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat("yyyy-MM-dd");
+        }
+        @Override
+        public SimpleDateFormat get() {
+            return super.get();
+        }
+    };
+
+    // Java 8
+    public static ThreadLocal<SimpleDateFormat> dateFormatThreadLocal1 = ThreadLocal.withInitial(() -> new SimpleDateFormat("yyyy-MM-dd"));
+
+    public class UserService {
+        public void main(String[] args) {
+
+        }
+
+        public String birthDate(int userId) {
+//            Date birthDate = birthDateFromDB( id );
+//            final SimpleDateFormat df = ThreadSafeFormatter.dateFormatThreadLocal.get();
+//            return df.format(birthDate);
+        }
+    }
+}
