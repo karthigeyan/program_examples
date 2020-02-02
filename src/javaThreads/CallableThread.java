fe1fb543d57342085a1172518a29d1e594b21f8a
v.karthigeyan@hotmail.com <[1flowersday1]>
2020-02-03 02:17:09 +0530

    Callable future

---
+++ src/javaThreads/CallableThread.java
@@ -0,0 +1,38 @@
+package javaThreads;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.*;
+
+public class CallableThread {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        List<Future> allFutures = new ArrayList<>();
+        for(int i=0; i<100; i++ ) {
+            Future<Integer> feature =  executorService.submit(new Task());
+            allFutures.add(feature);
+        }
+
+        //Perform Other operations
+
+        System.out.println("Future works");
+        for(int i=0; i<100; i++ ) {
+            Future<Integer> future = allFutures.get(i);
+            Integer result = future.get();
+            System.out.println("Result of future # " + i + " : " + result);
+        }
+        executorService.shutdown();
+    }
+
+    static class Task implements Callable<Integer> {
+
+        @Override
+        public Integer call() throws Exception {
+            Thread.sleep(3000 );
+            return new Random().nextInt();
+        }
+    }
+}
