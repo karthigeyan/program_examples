f7e2e9e1ff16eb6e375610558f734e6d72a596ed
karthigeyan vellasamy <karthigeyan.vella@pearson.com>
2020-02-17 13:35:57 +0530

    Design Pattern Structure

--- src/designPatterns/MainClass.java
+++ src/designPatterns/MainClass.java
@@ -3,6 +3,12 @@ package designPatterns;
 public class MainClass extends  SuperClass{
 
     public static void main(String[] args) {
+        MainClass mainClass =  new MainClass();
+        mainClass.getSample();
+    }
 
+    @Override
+    public void getSample() {
+        System.out.println("Main class sample override method");
     }
 }
--- src/designPatterns/SuperClass.java
+++ src/designPatterns/SuperClass.java
@@ -2,8 +2,13 @@ package designPatterns;
 
 public abstract class SuperClass implements SuperInterface {
 
+
+    public void getSample() {
+        System.out.println("Super class sample method call");
+    };
+
     @Override
     public void getMethod() {
-
+        System.out.println("Super class get method call");
     }
 }
